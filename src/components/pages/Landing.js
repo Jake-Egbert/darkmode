a04363db8d95a43d0f10881b35670f22d28a2948
Jake Egbert <jake@dp-c02sc0bug8wl.localdomain>
2023-08-31 11:25:41 -0600

    added main code for langauges

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,15 +1,28 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 
+import { Text, LanguageContext } from "../context/LanguageContext";
 import { DarkModeContext } from "../context/DarkModeContext";
 
 const Landing = () => {
   const { darkMode } = useContext(DarkModeContext);
+  const [clickText, setClickText] = useState();
+  const [selectedOption, setSelectedOption] = useState();
+  const { dictionary } = useContext(LanguageContext);
 
   const landingProvider = () => {
     if (darkMode) {
       return <p>darkmode</p>;
     }
     return <p>lightmode</p>;
+  };
+  const selectOptions = ["option1", "option2", "option3"];
+
+  const handleClick = () => {
+    setClickText(<Text tid="buttonClicked" />);
+  };
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
   };
 
   return (
@@ -18,6 +31,40 @@ const Landing = () => {
     >
       <div>welcome to the landing page</div>
       <div className="example-text">{landingProvider()}</div>
+      <div>
+        <h1>
+          <Text tid="exploreHeader" />
+        </h1>
+        <p>
+          <Text tid="welcomeDescription" />
+        </p>
+
+        <div>
+          <input type="text" placeholder={dictionary.enterText} />
+          <button onClick={handleClick}>
+            <Text tid="clickMe" />
+          </button>
+          <p>{clickText}</p>
+        </div>
+
+        <div>
+          <select onChange={handleOptionChange} value={selectedOption}>
+            {selectOptions.map((option) => (
+              <option key={option} value={option}>
+                {dictionary[option]}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <a
+          href="https://halilcanozcelik.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Text tid="aboutMe" />
+        </a>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import App from "./components/App";
 import ReactDOM from "react-dom/client";
 
+import { LanguageProvider } from "./components/context/LanguageContext";
 import { DarkModeProvider } from "./components/context/DarkModeContext";
 import "./styles/main.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
+    <LanguageProvider>
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
+    </LanguageProvider>
   </React.StrictMode>
 );
